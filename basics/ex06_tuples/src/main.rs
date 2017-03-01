e4bc5e12d8ca00e43637d1cfaede58913cbb1699
bjorn kihlberg <hoppfull@gmail.com>
2017-03-01 23:01:53 +0100

    implemented ex06_tuples example

---
+++ basics/ex06_tuples/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let t0 = (1, "hello"); // a tuple with two elements
+    println!("t0=({},{})", t0.0, t0.1); // (1, hello)
+    let t1 = (5,); // a tuple with one element
+    println!("t1=({})", t1.0) // (5)
+}
